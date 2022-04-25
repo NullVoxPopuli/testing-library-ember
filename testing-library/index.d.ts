5cb859011e16cbf7555eb62559e8790edddb4492
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-04-25 19:58:17 -0400

    feat(initial): Initial Implementation
    BREAKING CHANGE:
    from here on out, semver well be adhered to

--- testing-library/index.d.ts
+++ testing-library/index.d.ts
@@ -1,1 +1,14 @@
-export { fireEvent, screen } from '@testing-library/dom';
+export {fireEvent, screen} from '@testing-library/dom';
+// import type {fireEvent as tlFE, screen as tlS} from '@testing-library/dom';
+
+
+
+// export interface fireEvent {
+//   [K in keyof fireEvent]: (...Parameters<typeof tlFE[K]>) => Promise<ReturnType<tlFE[K]>>
+// }
+
+// The types for testing-library need to be manipulated somehow
+// we could do this manually.
+// But it'd be great to just "map" to promise
+//https://www.typescriptlang.org/play?#code/JYOwLgpgTgZghgYwgAgILIN4ChlwFzIAUMA9iQSAK4C2ARtAJTIC8AfMlXdANw60GFacKAQDOYKKADmTNsnGSQU3gF8sWMAE8ADigDSETaJbIA1oZIw0vLbuQBhOABsnQhKYA8AeSsQAHpAgACbGBkYmYaLszEQAdPHCUqIEAArCcNQQkFCiHqgA2j4Auqyy7ClQJNTAohAeAEpZlFAgACo6dQXFrKy8WKDZ8EjIAEKYOMj55prIoMiRRQSOLm6mqupOWci0JhgquMaUIKYgJADuIAejfbSxcDextNxAA
+
